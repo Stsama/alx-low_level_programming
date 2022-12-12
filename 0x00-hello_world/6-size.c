542bbc12d74de6d52a9b82c0f095bb366a1792c4
Stsama <alber>
2022-12-12 12:21:48 -0800

    true correction of 4 & 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	printf("size of an int is: i% byte(s)\n", sizeof(int));
 	printf("size of a long int is: i% byte(s)\n", sizeof(long int);
 	printf("size of a long long int is: i% byte(s)\n", sizeof(long long int);
-	printtf("size of a float is: i% byte(s)\n", sizeof(float);
+	printf("size of a float is: i% byte(s)\n", sizeof(float);
 	return (0);
 }
 
