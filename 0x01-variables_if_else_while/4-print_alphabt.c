722114b589fb6847d53fb24795d7570f9ac029cd
Stsama <alber>
2022-12-13 12:59:24 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry this program prints the alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al == 'e' || al == 'q')
+		{
+			continue;
+		}
+		putchar(al);
+	}
+	return (0);
+}
