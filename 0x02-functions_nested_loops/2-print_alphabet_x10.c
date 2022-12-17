6d118318099d28062d08f41a6c4b062efc1b411c
Stsama <alber>
2022-12-17 03:51:38 -0800

    task2 test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - repeats the print_alphabet 10 times.
+ * Return nothing
+*/
+void print_alphabet_x10(void)
+{
+	int x;
+
+	char letter;
+
+	for (x = 1; x <= 0; x++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
