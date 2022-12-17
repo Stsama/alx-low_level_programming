7b5be07d67f1cdfef6ac20dfe182d5981de8abdd
Stsama <alber>
2022-12-17 14:30:25 -0800

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - tests whether a character is
+ * positive or negative
+ * @n: character to test.
+ * Return: 1 if it is a positive, 0 if it is 0 and -1 if
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
